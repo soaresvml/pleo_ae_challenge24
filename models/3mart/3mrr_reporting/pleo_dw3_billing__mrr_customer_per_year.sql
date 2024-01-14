49559e6f947e76ce27a56013edf1a7b887e0613b
Vinicius <vinicius.soares@pleo.io>
2024-01-14 22:27:53 +0000

    marts time periods

---
+++ models/3mart/3mrr_reporting/pleo_dw3_billing__mrr_customer_per_year.sql
@@ -0,0 +1,11 @@
+select
+  date_trunc(report_date,year) as report_month
+  ,currency
+  ,customer_id
+  ,sum(amount_per_day) as amount
+  ,sum(amount_eur_per_day) as amount_eur_per_day
+from
+  {{ ref('pleo_dw3_billing__mrr_customer_per_day') }}
+group by 
+  1,2,3
+order by 1
